4cab00fbfecefbe164fa4729b64b4c27e31a2197
Leonardo Moreira Cunha <leonardomoreiraemp@gmail.com>
2025-08-10 22:36:30 -0300

    new 2

--- src/pages/api/bff/teste-cadastro.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log('Método recebido:', req.method);
-  if (req.method === 'POST') {
-    return res.status(200).json({ message: 'Requisição POST recebida com sucesso!' });
-  }
-  return res.status(405).json({ message: 'Method Not Allowed' });
-}
